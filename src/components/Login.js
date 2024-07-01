626021236ddb6846d541f8c25f8e5c859781f212
Sarthak Namdev <118155374+Srthk-08@users.noreply.github.com>
2024-07-01 16:30:11 +0530

    c

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,12 +25,12 @@ const Login = () => {
             <form onSubmit={handleLogin}>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" name='email' value={credentials.email} aria-describedby="emailHelp" onChange={onchange} />
+                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onchange}  value={credentials.email}/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name='password' value={credentials.password} onchange={onChange} />
+                    <input type="password" className="form-control" id="password" name='password'  onchange={onChange}  value={credentials.password}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
